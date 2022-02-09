3deb2595434f1472752618894a86ce663ba30888
IGDERZEN-Zakaria <zakaria.igderzen@gmail.com>
2022-02-09 16:15:38 +0100

    custom exception

--- PPMToolFullStack/ppmtool/src/main/java/io/kepler/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/kepler/ppmtool/services/ProjectService.java
@@ -1,19 +1,26 @@
 package io.kepler.ppmtool.services;
 
+
 import io.kepler.ppmtool.domain.Project;
+import io.kepler.ppmtool.exceptions.ProjectIdException;
 import io.kepler.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectService {
+
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject (Project project) {
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
 
-        //Logic
-
-        return projectRepository.save(project);
     }
+
 }
